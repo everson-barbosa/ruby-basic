cacce91ecb625cc8e74c7800cad9c22245fd0734
eversilva <everson.silva2706@gmail.com>
2022-09-18 23:48:00 -0300

    STDIN example

---
+++ commands/input.rb
@@ -0,0 +1,6 @@
+puts "Digite o seu nome: "
+firstname = gets.chomp # Everson
+puts "Digite a seu sobrenome: "
+lastname = gets.chomp # Silva
+puts "O seu nome é #{firstname} #{lastname}" # Everson Silva
+# o método chomp do get serve para não trazer a string com quebra de linha
