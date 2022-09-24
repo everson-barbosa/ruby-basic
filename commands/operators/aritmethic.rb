ad8a502c055db322a1cc74488bf936a282041b6d
eversilva <everson.silva2706@gmail.com>
2022-09-23 23:59:57 -0300

    Operators aritmethic

---
+++ commands/operators/aritmethic.rb
@@ -0,0 +1,17 @@
+# + : Soma
+puts("Soma: 9 + 2 = #{9 + 2}")
+
+# - : Subtração
+puts("Subtração: 9 - 2 = #{9 - 2}")
+
+# / : Divisão
+puts("Subtração: 9 / 2 = #{9 / 2}")
+
+# * : Multiplicação
+puts("Subtração: 9 * 2 = #{9 * 2}")
+
+# * : Potencial
+puts("Potencial: 9 ** 2 = #{9 ** 2}")
+
+# * : Resto da divisão
+puts("Resto da divisão: 9 % 2 = #{9 % 2}")
