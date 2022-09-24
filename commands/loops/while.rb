9eebb09270f49ed0d2e020aa8089561d96af0d1a
eversilva <everson.silva2706@gmail.com>
2022-09-24 00:10:30 -0300

    Loops while

---
+++ commands/loops/while.rb
@@ -0,0 +1,9 @@
+# While / Laço de repetição usado amplamente na programação, enquanto a condição for false ele vai repetir o laço
+
+i = 0
+text = 'Bora bill!'
+
+while i < text.length do
+    puts text[0, i+1]
+    i += 1
+end
