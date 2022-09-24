731370c0af35923cb2af41903a8a96b1500ac11b
eversilva <everson.silva2706@gmail.com>
2022-09-24 00:01:38 -0300

    Adjust relational puts

--- commands/operators/relational.rb
+++ commands/operators/relational.rb
@@ -1,18 +1,18 @@
-# > / Maior que
-puts("2 > 1 #{2 > 1}, 1 > 2 #{1 > 2}")
+# > : Maior que
+puts("Maior que: 2 > 1 #{2 > 1}, Maior que: 1 > 2 #{1 > 2}")
 
-# >= / Maior ou igual que
-puts("2 >= 1 #{2 >= 1}, 1 >= 2 #{1 >= 2}")
+# >= : Maior ou igual que
+puts("Maior ou igual que: 2 >= 1 #{2 >= 1}, Maior ou igual que: 1 >= 2 #{1 >= 2}")
 
-# < / Menor que
-puts("2 < 1 #{2 < 1}, 1 < 2 #{1 < 2}")
+# < : Menor que
+puts("Menor que: 2 < 1 #{2 < 1}, Menor que: 1 < 2 #{1 < 2}")
 
-# =< / Menor ou igual que
-puts("2 <= 1 #{2 <= 1}, 1 <= 2 #{1 <= 2}")
+# =< : Menor ou igual que
+puts("Menor ou igual que: 2 <= 1 #{2 <= 1}, Menor ou igual que: 1 <= 2 #{1 <= 2}")
 
-# == / Igual que
-puts("2 == 1 #{2 == 1}, 1 == 2 #{1 == 2}")
+# == : Igual que
+puts("Igual que: 2 == 1 #{2 == 1}, Igual que: 1 == 2 #{1 == 2}")
 
-# != / Diferente que
-puts("2 != 1 #{2 != 1}, 1 != 2 #{1 != 2}")
+# != : Diferente que
+puts("Diferente que: 2 != 1 #{2 != 1}, Diferente que: 1 != 2 #{1 != 2}")
 
