fbfda79e92b6223176b343ecf9346c50c205fd66
eversilva <everson.silva2706@gmail.com>
2022-09-23 23:56:37 -0300

    Operators relational

---
+++ commands/operators/relational.rb
@@ -0,0 +1,18 @@
+# > / Maior que
+puts("2 > 1 #{2 > 1}, 1 > 2 #{1 > 2}")
+
+# >= / Maior ou igual que
+puts("2 >= 1 #{2 >= 1}, 1 >= 2 #{1 >= 2}")
+
+# < / Menor que
+puts("2 < 1 #{2 < 1}, 1 < 2 #{1 < 2}")
+
+# =< / Menor ou igual que
+puts("2 <= 1 #{2 <= 1}, 1 <= 2 #{1 <= 2}")
+
+# == / Igual que
+puts("2 == 1 #{2 == 1}, 1 == 2 #{1 == 2}")
+
+# != / Diferente que
+puts("2 != 1 #{2 != 1}, 1 != 2 #{1 != 2}")
+
