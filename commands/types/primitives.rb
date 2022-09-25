109db7baa79626639a4cddbe836b28125504139f
eversilva <everson.silva2706@gmail.com>
2022-09-24 21:26:49 -0300

    Types primitives

---
+++ commands/types/primitives.rb
@@ -0,0 +1,9 @@
+# Tipos primitivos: 
+puts "
+    Tipos primitivos
+    ------------------------------
+    Inteiro - Integer: 10
+    Real - Float: 3.45
+    Caracteres - String: 'Everson'
+    Lógico: Boolean: true
+    "
