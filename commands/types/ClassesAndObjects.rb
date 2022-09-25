e4ca284250505c9f5bd3de77a0a8e6fced1a8721
eversilva <everson.silva2706@gmail.com>
2022-09-24 23:36:01 -0300

    types Classes and Objects

---
+++ commands/types/ClassesAndObjects.rb
@@ -0,0 +1,9 @@
+# Classes e Objetos: A partir da classe podemos gerar objetos.
+
+puts "Classes e Objetos"
+
+puts 
+    "
+        Quando declaramos um inteiro, ele gera a variavel como um objeto da classe Integer.
+        Tente abrir o IRB ou PRY e digite 10.class
+    "
