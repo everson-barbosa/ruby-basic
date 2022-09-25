735d34f17f8544c3613394d4a5c3b6a1a56a30dc
eversilva <everson.silva2706@gmail.com>
2022-09-25 00:56:34 -0300

    Add methods to class

--- commands/types/ClassesAndObjects.rb
+++
@@ -1,9 +0,0 @@
-# Classes e Objetos: A partir da classe podemos gerar objetos.
-
-puts "Classes e Objetos"
-
-puts 
-    "
-        Quando declaramos um inteiro, ele gera a variavel como um objeto da classe Integer.
-        Tente abrir o IRB ou PRY e digite 10.class
-    "
---
+++ commands/types/classes.rb
@@ -0,0 +1,17 @@
+# Classes e Objetos: A partir da classe podemos gerar objetos.
+# Exemplo: A Classe Pokemon pode gerar Dragonite, Snorlax, Vaporeon, todos são pokemons, mas como informações diferentes, ou seja, cada um deles é um objeto da classe Pokemon.
+
+class Pokemon
+    def action(move = "Struggle")
+        "Pokemon usou o #{move}"
+    end
+    def recoverHP(currentHP = 0, recoveredHP = 0)
+        "HP recuperado para: #{currentHP.to_i + recoveredHP.to_i}"
+    end
+end
+
+mew = Pokemon.new
+puts mew.action
+puts mew.action("Psystrike")
+puts mew.recoverHP
+puts mew.recoverHP(40, 50)
